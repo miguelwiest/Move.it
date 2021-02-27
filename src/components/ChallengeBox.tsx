f12143a696f5682ff7177581e8b5cf4cdc14eded
miguelwiest <69208528+miguelwiest@users.noreply.github.com>
2021-02-27 00:31:37 -0300

    Mais um

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import { CountdownContext } from '../contexts/CountdownContext';
+
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,8 @@
 import '../styles/global.css';
 
-import { ChallengesProvider } from '../contexts/ChallengesContext'
-import React from 'react';
-import { CountdownProvider } from '../contexts/CountdownContext';
-
 function MyApp({ Component, pageProps }) {
   return (
-  <ChallengesProvider>
     <Component {...pageProps} />
-  </ChallengesProvider>
   )
 }
 
